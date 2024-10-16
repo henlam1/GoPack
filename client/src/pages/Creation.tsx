10e1098104c6a359f4ababb03f67e4da10cb6daa
henlam <henry.lam.1@stonybrook.edu>
2024-10-15 23:22:50 -0400

    Creation form

--- client/src/pages/Creation.tsx
+++ client/src/pages/Creation.tsx
@@ -1,12 +1,26 @@
-type CreationProps = {
-    onFinish: () => void;
-}
+import { useNavigate } from "react-router-dom"
 
-export default function Creation(props: CreationProps) {
+export default function Creation() {
+    const navigate = useNavigate();
     return(
-        <div>
-            Creation Page
-            <button className="btn" onClick={props.onFinish}>CLICK ME</button>
+        <div className="card w-96 shadow-xl card-bordered mx-auto">
+            <div className="card-body items-center text-center">
+                <div className="card-title">Creation Page</div>
+                <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">Packing List Name</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="label">
+                    <span className="label-text">Trip Duration</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                </label>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary" onClick={() => navigate("/")}>Ok</button>
+                    <button className="btn btn-secondary" onClick={() => navigate("/")}>Cancel</button>
+                </div>
+            </div>
         </div>
     )
 }
