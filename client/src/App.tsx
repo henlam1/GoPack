69a3e305c122f63651ef48a5f37e6e530832ca05
Cindy Lam <cindylam@Cindys-MacBook-Pro.local>
2024-10-16 01:35:57 -0400

    adding tailwind forms and packing list component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import Landing from "./pages/Landing";
 import Creation from "./pages/Creation";
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PackingList from './pages/PackingList';
+import PackingListPage from './pages/PackingListPage';
 import Layout from "./components/Layout";
 
 
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Landing />} />
           <Route path="/create" element={<Creation />} />
-          <Route path="/packing-list/:id" element={<PackingList />} />
+          <Route path="/packing-list/:id" element={<PackingListPage />} />
         </Route>
       </Routes>
     </Router>
---
+++ client/src/components/PackingList.tsx
@@ -0,0 +1,159 @@
+export default function PackingList() {
+    return (
+        <div className="flex flex-wrap gap-4">
+            <div className="card shadow-xl card-bordered mx-auto">
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 1</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 2</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 3</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 4</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+
+            </div>
+            <div className="card shadow-xl card-bordered mx-auto">
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 1</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 2</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 3</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 4</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+
+            </div>
+            <div className="card shadow-xl card-bordered mx-auto">
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 1</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 2</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 3</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 4</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+
+            </div>
+            <div className="card shadow-xl card-bordered mx-auto">
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 1</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 2</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 3</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 4</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+
+            </div>
+            <div className="card shadow-xl card-bordered mx-auto">
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 1</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 2</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 3</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 4</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+
+            </div>
+            <div className="card shadow-xl card-bordered mx-auto">
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 1</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 2</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 3</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 4</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+
+            </div>
+            <div className="card shadow-xl card-bordered mx-auto">
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 1</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 2</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 3</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 4</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+
+            </div>
+            <div className="card shadow-xl card-bordered mx-auto">
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 1</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 2</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 3</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+                <label className="cursor-pointer label">
+                    <span className="label-text">Remember me 4</span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                </label>
+
+            </div>
+        </div>
+ 
+    );
+}
