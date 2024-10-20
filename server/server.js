c1d67f205735d9e60dd2b6f3a8e8f64af0cdd4d4
Cindy Lam <cllam@users.noreply.github.com>
2024-10-19 22:18:18 -0400

    database stuff

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import packinglist from "./routes/packinglist.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/packinglist", packinglist);
+
 
 // start the Express server
 app.listen(PORT, () => {
