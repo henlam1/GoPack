bd488a6cd2531ba26a96b6d14a11c90336982910
henlam <89503456+henlam1@users.noreply.github.com>
2024-10-30 22:14:06 -0400

    Added basic form for adding items

--- client/src/components/PackingList.tsx
+++ client/src/components/PackingList.tsx
@@ -6,16 +6,16 @@ interface PackingListProps {
 }
 
 export default function PackingList(props: PackingListProps) {
-    console.log(props.categories);
+    console.log(props);
 
     const categoryList = props.categories.map( (category, index) => {
         const link = `#item${index+1}`
-        return <li><a href={link}>{category.name}</a></li>
+        return <li key={index}><a href={link}>{category.name}</a></li>
     })
     return (
         <div className="">
             <ul className="menu bg-base-200 rounded-box sticky top-0 float-left w-3/12">
-                <li className="menu-title uppercase">Categories</li>
+                <li key="title" className="menu-title uppercase">Categories</li>
                 {categoryList}
 
             </ul>
