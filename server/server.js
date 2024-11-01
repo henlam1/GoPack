80e6098d8baaa1fdb14552780efe22ce42c8c86c
Cindy Lam <cindylam@cindys-mbp.lan>
2024-10-31 18:18:19 -0700

    some backend stuff

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import packingListRouter from "./routes/packingList.js";
 import categoryListRouter from "./routes/categoryList.js";
+import itemListDefaultsRouter from "./routes/itemListDefaults.js";
+import categoryTypeRouter from "./routes/categoryType.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -10,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/packingList", packingListRouter);
 app.use("/categoryList", categoryListRouter);
+app.use("/itemListDefaults", itemListDefaultsRouter);
+app.use("/categoryType", categoryTypeRouter);
 
 
 // start the Express server
