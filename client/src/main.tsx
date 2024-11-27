bf25e28e5b90fddd815883dec40635fe0a09c9c9
henlam <89503456+henlam1@users.noreply.github.com>
2024-11-27 13:34:08 -0500

    Added google log in

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.tsx'
 import './index.css'
 
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+console.log(CLIENT_ID);
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>,
 )
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+import { googleLogout } from '@react-oauth/google';
 import AllPackingLists from "../features/packingList/AllPackingLists";
+import GoogleSignIn from "../components/GoogleSignIn";
 import { useNavigate } from 'react-router-dom';
+import User from '../components/User';
 
 export default function Landing(){
+    const [ profile, setProfile ] = useState(null);
     const navigate = useNavigate();
+
+    const logout = () => {
+        googleLogout();
+        setProfile(null);
+    }
 
     return(
         <div className="text-center">
@@ -17,6 +27,19 @@ export default function Landing(){
                     >
                         Add New
                     </button>
+                    {profile ? (
+                        <User
+                            name={profile.name}
+                            email={profile.email}
+                            picture={profile.picture}
+                            logOut={logout}
+                        />
+                        ) : (
+                        <GoogleSignIn
+                            setProfile={setProfile}
+                        />
+                        )}
+                    
                 </div>
             </div>
         </div>
