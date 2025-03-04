a67d4f1e504e975d21f3d97d1f235448a7aa3be1
henlam <henry.lam.1@stonybrook.edu>
2025-03-04 02:16:38 -0500

    Added starter files in each folder. Created simple routes for display

---
+++ client/src/containers/PackingListContainer.tsx
@@ -0,0 +1,9 @@
+export default function PackingListContainer() {
+  // TODO: Decide what containers we need
+  // e.g. containers for showing packing lists, 
+  // accordian for categories
+  // container for grouping items
+  return(<div>
+    Apples
+  </div>)
+}
