9b03aa13d6cb03a2af3c7e2c6c4b1b69e2b81b27
henlam <89503456+henlam1@users.noreply.github.com>
2025-03-11 14:32:56 -0400

    Updated services with basic CRUD

--- server/services/categoryService.js
+++ server/services/categoryService.js
@@ -4,9 +4,24 @@ class CategoryService {
   async getCategories() {
     return await Category.find();
   }
+  
   async addCategory(data) {
     const newCategory = new Category(data);
     return await newCategory.save();
+  }
+  
+  async updateCategory(data) {
+    const { filter, update } = data;
+    const updatedCategory = await Category.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+    return updatedCategory;
+  }
+  
+  async deleteCategory(data) {
+    const { filter } = data;
+    const deletedCategory = await Category.findOneAndDelete(filter);
+    return deletedCategory;
   }
 }
 
--- server/services/packingListService.js
+++ server/services/packingListService.js
@@ -4,9 +4,24 @@ class PackingListService {
   async getPackingLists() {
     return await PackingList.find();
   }
+  
   async addPackingList(data) {
     const newList = new PackingList(data);
     return await newList.save();
+  }
+  
+  async updatePackingList(data) {
+    const { filter, update } = data;
+    const updatedPackingList = await PackingList.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+    return updatedPackingList;
+  }
+  
+  async deletePackingList(data) {
+    const { filter } = data;
+    const deletedPackingList = await PackingList.findOneAndDelete(filter);
+    return deletedPackingList;
   }
 }
 
