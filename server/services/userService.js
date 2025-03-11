6f0176029fd5ed0da022421bd1a35e0c1322d741
henlam <henry.lam.1@stonybrook.edu>
2025-03-11 14:32:56 -0400

    Updated services with basic CRUD

--- server/services/userService.js
+++ server/services/userService.js
@@ -9,6 +9,20 @@ class UserService {
     const user = new User(data);
     return await user.save();
   }
+
+  async updateUser(data){
+    const { filter, update } = data;
+    const updatedUser = await User.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+    return updatedUser;
+  }
+  
+  async deleteUser(data) {
+    const { filter } = data;
+    const deletedUser = await User.findOneAndDelete(filter);
+    return deletedUser;
+  }
 }
 
 export default new UserService();
