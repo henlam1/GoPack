a964855108c5d2acf07701768bf985f3a6955cda
henlam <henry.lam.1@stonybrook.edu>
2025-03-12 01:07:05 -0400

    Updated Items to query by id

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -3,31 +3,28 @@ import ItemService from "../services/itemService.js";
 
 export const getItems = tryCatch(async (req, res, next) => {
   const items = await ItemService.getItems();
-  res.status(200).json({ items: items });
+  res.status(200).json(items);
+});
+
+export const getItemById = tryCatch(async (req, res, next) => {
+  const { itemId } = req.params;
+  const item = await ItemService.getItemById(itemId);
+  res.status(200).json(item);
 });
 
 export const addItem = tryCatch(async (req, res, next) => {
   const newItem = await ItemService.addItem(req.body);
-  res.status(201).json({
-    message: "Item added",
-    item: newItem,
-  });
+  res.status(201).json(newItem);
 });
 
 export const updateItem = tryCatch(async (req, res, next) => {
   const { itemId } = req.params;
   const updatedItem = await ItemService.updateItem(itemId, req.body);
-  res.status(200).json({
-    message: "Item updated",
-    item: updatedItem,
-  });
+  res.status(200).json(updatedItem);
 });
 
 export const deleteItem = tryCatch(async (req, res, next) => {
   const { itemId } = req.params;
   const deletedItem = await ItemService.deleteItem(itemId);
-  res.status(200).json({
-    message: "Item deleted",
-    item: deletedItem,
-  });
+  res.status(200).json(deletedItem);
 });
