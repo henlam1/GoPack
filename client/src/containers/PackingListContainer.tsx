ebeac9cc84c49130edc82dbe6e6e6ee47e70a2c8
henlam <89503456+henlam1@users.noreply.github.com>
2025-03-14 01:25:00 -0400

    Creating fetch APIs for Items

--- client/src/containers/PackingListContainer.tsx
+++ client/src/containers/PackingListContainer.tsx
@@ -1,40 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import IItem from "../models/ItemModel";
+import { getItems } from "../services/api/items";
+
 export default function PackingListContainer() {
   // TODO: Decide what containers we need
   // e.g. containers for showing packing lists,
   // accordian for categories
   // container for grouping items
+
+  // TODO: DRY Principle
+  // Current rendering is egregious and nested
+  const {
+    data: items,
+    isError,
+    isPending,
+  } = useQuery({
+    queryKey: ["items"],
+    queryFn: getItems,
+  });
   return (
     <>
-      <div className="collapse collapse-arrow bg-base-100 border-base-300 border">
-        <input type="checkbox" />
-        <div className="collapse-title font-semibold">
-          How do I create an account?
-        </div>
-        <div className="collapse-content text-sm">
-          Click the "Sign Up" button in the top right corner and follow the
-          registration process.
-        </div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-100 border-base-300 border">
-        <input type="checkbox" />
-        <div className="collapse-title font-semibold">
-          How do I create an account?
-        </div>
-        <div className="collapse-content text-sm">
-          Click the "Sign Up" button in the top right corner and follow the
-          registration process.
-        </div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-100 border-base-300 border">
-        <input type="checkbox" />
-        <div className="collapse-title font-semibold">
-          How do I create an account?
-        </div>
-        <div className="collapse-content text-sm">
-          Click the "Sign Up" button in the top right corner and follow the
-          registration process.
-        </div>
-      </div>
+      {isPending && <div>Loading...</div>}
+      {isError && <div>Error...</div>}
+      {!isPending && (
+        <ul>
+          {items.map((item: IItem) => {
+            return (
+              <div className="collapse collapse-arrow bg-base-100 border-base-300 border">
+                <input type="checkbox" />
+                <div className="collapse-title font-semibold">
+                  How do I create an account?
+                </div>
+                <div className="collapse-content text-sm">
+                  {item.packed}
+                  {item.name}
+                  {item.quantity}
+                </div>
+              </div>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 }
---
+++ client/src/services/api/items.ts
@@ -0,0 +1,50 @@
+import IItem from "../../models/ItemModel";
+import { apiRoutes } from "../../routes/apiRoutes";
+
+export async function getItems() {
+  try {
+    const response = await fetch(apiRoutes.items.getAll);
+    if (!response.ok) {
+      throw new Error("Failed to create item");
+    }
+    const data = await response.json();
+    console.log("Data fetched: ", data);
+    return data;
+  } catch (error) {
+    console.error("Fetch error: ", error);
+  }
+}
+
+export async function getItem(id: string) {
+  try {
+    const response = await fetch(apiRoutes.items.getById(id));
+    if (!response.ok) {
+      throw new Error("Failed to create item");
+    }
+    const data = await response.json();
+    console.log("Data fetched: ", data);
+    return data;
+  } catch (error) {
+    console.error("Fetch error: ", error);
+  }
+}
+
+export async function createItem(item: IItem) {
+  try {
+    const response = await fetch(apiRoutes.items.create, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to create item");
+    }
+    const data = await response.json();
+    console.log("Data fetched: ", data);
+    return data;
+  } catch (error) {
+    console.error("Fetch error: ", error);
+  }
+}
