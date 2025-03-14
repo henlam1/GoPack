b5ce30fd0128f0bb5b9c2a718bd767d6e2dd2811
henlam <henry.lam.1@stonybrook.edu>
2025-03-14 15:15:20 -0400

    Added containers & components for packinglists, categorys, items

---
+++ client/src/components/PackingList.tsx
@@ -0,0 +1,7 @@
+export default function PackingList(){
+  return(
+    <>
+    I'm a packing list
+    </>
+  )
+}
---
+++ client/src/containers/CategoryContainer.tsx
@@ -0,0 +1,10 @@
+// CONTAINERS ARE RESPONSIBLE FOR MANAGING STATE AND PASSING DATA TO CHILD COMPONENTS
+// CategoryContainer => Fetch Categories => Render Category(props)
+// This is used in the packing list page to display categories in the accordion
+export default function CategoryContainer() {
+  return(
+    <>
+    Some categories
+    </>
+  )
+}
