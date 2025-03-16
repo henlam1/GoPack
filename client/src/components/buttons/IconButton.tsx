163945ce4f9e386579844e5bb111fe126c59f743
henlam <89503456+henlam1@users.noreply.github.com>
2025-03-15 21:12:49 -0400

    Added form with react-hook-forms and zod

---
+++ client/src/components/buttons/IconButton.tsx
@@ -0,0 +1,16 @@
+interface IconButtonProps {
+  svgIcon: React.ReactNode;
+  text: string;
+}
+
+export default function IconButton({
+  svgIcon,
+  text,
+}: IconButtonProps) {
+  return (
+    <button className="btn">
+      {svgIcon}
+      {text}
+    </button>
+  );
+}
