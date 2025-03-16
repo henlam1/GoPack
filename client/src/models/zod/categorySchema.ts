51309fd91f92c6227cfc4250836338b36b7a7c9c
henlam <henry.lam.1@stonybrook.edu>
2025-03-16 15:46:21 -0400

    Created base files for category and packing list forms

---
+++ client/src/models/zod/categorySchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { itemSchema } from "./itemSchema";
+import { genHexString } from "../../utils/stringHelpers";
+
+export const categorySchema = z.object({
+  name: z.string().min(1).max(30),
+  items: z.array(itemSchema),
+  packingList: z.string()
+});
+
+export type CategoryFormFields = z.infer<typeof categorySchema>;
+
+export const categoryDefaults = {
+  items: [],
+  packingList: genHexString(24),
+}
---
+++ client/src/models/zod/packingListSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { categorySchema } from "./categorySchema";
+import { genHexString } from "../../utils/stringHelpers";
+
+export const packingListSchema = z.object({
+  name: z.string().min(1).max(30),
+  categories: z.array(categorySchema),
+  user: z.string(),
+});
+
+export type PackingListFormFields = z.infer<typeof packingListSchema>;
+
+export const packingListDefaults = {
+  categories: [],
+  user: genHexString(24),
+};
