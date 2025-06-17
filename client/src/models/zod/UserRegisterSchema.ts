5883a968e35b8efcef1d23ddd0c866413a087a90
Cindy Lam <cindylam@Cindys-MacBook-Pro.local>
2025-06-16 23:44:08 -0400

    getting some form validation on register form and adding theme toggle

---
+++ client/src/models/zod/UserRegisterSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const UserRegisterSchema = z.object({
+  username: z
+    .string()
+    .min(1, {message: "Username must be min of 1 characters"})
+    .max(30, {message: "Username must be max of 30 characters"}),
+  password: z
+    .string()
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,30}$/, {message: "Password needs to be between 8 and 30 characters and have numbers and letters"}),
+  email: z
+    .string()
+    .regex(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, {message: "Not a valid email"}),
+});
+
+export type RegisterFormFields = z.infer<typeof UserRegisterSchema>;
+
+// export const registerDefaults = {
+//   name: "",
+//   password: "",
+//   email: ""
+// }
--- client/src/pages/public/RegisterPage.tsx
+++ client/src/pages/public/RegisterPage.tsx
@@ -1,24 +1,13 @@
+import RegisterForm from "../../components/forms/RegisterForm";
+
 export default function RegisterPage() {
+
   return (
     <div className="flex justify-center items-center">
-      <div className="card card-border bg-accent w-96 shadow-lg">
+      <div className="card card-border bg-primary w-96 shadow-lg">
         <div className="card-body items-center">
-          <h2 className="card-title text-4xl font-bold mb-5 text-accent-content">Register</h2>
-          <label className="floating-label">
-            <span>Username</span>
-            <input type="text" placeholder="username" className="input input-md" />
-          </label>          
-          <label className="floating-label">
-            <span>Password</span>
-            <input type="text" placeholder="password" className="input input-md" />
-          </label>
-          <label className="floating-label">
-            <span>Email</span>
-            <input type="text" placeholder="mail@site.com" className="input input-md" />
-          </label>
-          <div className="card-actions">
-            <button className="btn btn-primary">Register</button>
-          </div>
+          <h2 className="card-title text-4xl font-bold mb-5 text-primary-content">Register</h2>
+          <RegisterForm />
         </div>
       </div>
     </div>
