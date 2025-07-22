9606102883695172d90494006baa95312d109f6f
henlam <henry.lam.1@stonybrook.edu>
2025-07-22 14:57:30 -0400

    token controller tests

---
+++ server/tests/controllers/tokenController.test.js
@@ -0,0 +1,31 @@
+import request from "supertest";
+import testApp from "../testApp";
+import { createMockUser } from "../helpers/createMockData";
+import TokenService from "../../services/TokenService";
+
+describe("POST /refresh", () => {
+  const user = createMockUser();
+
+  it("should generate a new access token", async () => {
+    const accessToken = await TokenService.generateAccessToken(user);
+    const refreshToken = await TokenService.generateRefreshToken(user);
+    const res = await request(testApp)
+      .post("/api/tokens/refresh")
+      .set("Cookie", [
+        `accessToken=${accessToken}`,
+        `refreshToken=${refreshToken}`,
+      ]);
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe("Token refreshed successfully")
+  });
+
+  it("should not generate a new access token with a missing refresh token", async () => {
+    const accessToken = await TokenService.generateAccessToken(user);
+    const res = await request(testApp)
+      .post("/api/tokens/refresh")
+      .set("Cookie", [
+        `accessToken=${accessToken}`,
+      ]);
+    expect(res.status).toBe(401);
+  });
+});
