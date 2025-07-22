950fc000e6c05e13673dfa80dcc87a916de4a81b
henlam <henry.lam.1@stonybrook.edu>
2025-07-22 15:04:38 -0400

    auth middleware tests

---
+++ server/tests/middlewares/authMiddleware.test.js
@@ -0,0 +1,29 @@
+import request from "supertest";
+import app from "../../app";
+import { createMockItem, createMockUser } from "../helpers/createMockData";
+import TokenService from "../../services/TokenService";
+
+describe("Authentication middleware", () => {
+  const user = createMockUser();
+  const mockItem = createMockItem();
+
+  it("should create an item if authenticated", async () => {
+    const accessToken = await TokenService.generateAccessToken(user);
+    const refreshToken = await TokenService.generateRefreshToken(user);
+    const res = await request(app)
+      .post("/api/tokens/refresh")
+      .set("Cookie", [
+        `accessToken=${accessToken}`,
+        `refreshToken=${refreshToken}`,
+      ])
+      .send(mockItem);
+    expect(res.status).toBe(200);
+  });
+
+  it("should not create an item without authentication", async () => {
+    const res = await request(app)
+      .post("/api/tokens/refresh")
+      .send(mockItem);
+    expect(res.status).toBe(401);
+  });
+});
