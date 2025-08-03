67ff604925566cfbe926745848b56607bffb2f35
henlam <henry.lam.1@stonybrook.edu>
2025-08-03 13:27:51 -0400

    Testing workflow bug fix

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import 'dotenv/config';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import routes from './routes/index.js';
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import connectDB from './config/db.js';
 import app from './app.js';
 
