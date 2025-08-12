a255dfe8cd02620cd0cd2cfad9ab084f4e3761a1
henlam <henry.lam.1@stonybrook.edu>
2025-08-12 19:23:15 -0400

    Cookie samesite bug

--- server/services/tokenService.js
+++ server/services/tokenService.js
@@ -18,13 +18,14 @@ class TokenService {
   getSameSite() {
     const { isProd } = getEnv();
     const sameSite = isProd ? false : true;
+    console.log(sameSite);
     return sameSite;
   }
 
   setAccessToken(res, accessToken) {
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
-      sameSite: this.getSameSite(),
+      sameSite: false,
       secure: true,
       maxAge: 15 * 60 * 1000, // 15 minutes
     });
