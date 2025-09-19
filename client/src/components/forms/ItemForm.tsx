fbfa1d3971f701192d2b682be3c6fdc59f3a65f3
henlam <henry.lam.1@stonybrook.edu>
2025-09-19 14:33:39 -0400

    Include checkbox in hook form

--- client/src/components/forms/ItemForm.tsx
+++ client/src/components/forms/ItemForm.tsx
@@ -50,9 +50,10 @@ export default function ItemForm({ categoryId, children }: ItemFormProps) {
           {/* Form Table */}
           <tr>
             <td>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-primary disabled"
+              <FormInput
+                label=""
+                {...register('packed')}
+                error={errors.packed?.message}
               />
             </td>
             <td>
@@ -85,67 +86,5 @@ export default function ItemForm({ categoryId, children }: ItemFormProps) {
         </tbody>
       </table>
     </form>
-
-    // <>
-    //   {showForm && (
-    //     <form
-    //       onSubmit={handleSubmit(onSubmit)}
-    //       className="grid grid-cols-3 gap-4"
-    //     >
-    //       <span>
-    //         <input
-    //           {...register('name')}
-    //           type="text"
-    //           placeholder="Item name"
-    //           className="input validator"
-    //         />
-    //         {errors.name && (
-    //           <p className="block text-error">{errors.name.message}</p>
-    //         )}
-    //       </span>
-
-    //       <span>
-    //         <input
-    //           {...register('quantity')}
-    //           type="number"
-    //           className="input validator"
-    //           min="1"
-    //         />
-    //         {errors.quantity && (
-    //           <p className="block text-errort">{errors.quantity.message}</p>
-    //         )}
-    //       </span>
-
-    //       <span className="flex gap-1">
-    //         <button className="btn btn-square btn-success text-2xl">
-    //           {isSubmitting ? '...' : '+'}
-    //         </button>
-    //         <button
-    //           className="btn btn-square btn-neutral"
-    //           onClick={(e) => {
-    //             reset();
-    //             e.stopPropagation();
-    //             toggleShowForm();
-    //           }}
-    //         >
-    //           <svg
-    //             xmlns="http://www.w3.org/2000/svg"
-    //             fill="currentColor"
-    //             viewBox="0 0 24 24"
-    //             className="size-[2em]"
-    //             stroke="currentColor"
-    //           >
-    //             <path d="M13.775,18.707,8.482,13.414a2,2,0,0,1,0-2.828l5.293-5.293,1.414,1.414L9.9,12l5.293,5.293Z" />
-    //           </svg>
-    //         </button>
-    //       </span>
-    //     </form>
-    //   )}
-    //   {!showForm && (
-    //     <button className="btn btn-primary" onClick={toggleShowForm}>
-    //       {isSubmitting ? '...' : '+ Item'}
-    //     </button>
-    //   )}
-    // </>
   );
 }
